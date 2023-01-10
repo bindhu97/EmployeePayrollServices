9a3d10bf7a25a1ace322f02641332264c41f4f98
bindhu97 <binduvishwakarma97@gmail.com>
2023-01-10 23:41:56 +0530

    [Bindhu]Add: Ability to create Employee Payroll Table

--- EmployeePayrollServices/EmployeePayroll.sql
+++ EmployeePayrollServices/EmployeePayroll.sql
@@ -1,1 +1,5 @@
 Create Database EmployeePayroll;
+
+Create Table EmployeePayrollTable(ID bigint Identity(1,1)Primary Key, Name varchar(100), Salary bigint, Start date)
+
+select * from EmployeePayrollTable
