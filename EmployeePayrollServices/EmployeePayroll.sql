a1226787c71a132e5e8efa097ab4e059c086662c
bindhu97 <binduvishwakarma97@gmail.com>
2023-01-10 23:59:47 +0530

    [Bindhu]Add: Ability to Retrieve Employee Data

--- EmployeePayrollServices/EmployeePayroll.sql
+++ EmployeePayrollServices/EmployeePayroll.sql
@@ -6,3 +6,5 @@ select * from EmployeePayrollTable
 
 Insert into EmployeePayrollTable values ('Alex', 25000, '2020-02-05'), ('Sunil', 30000, '2018-01-01'), ('Meena', 35000, '2019-05-05'), ('Asha', 40000, '2018-08-04');
 
+select * from EmployeePayrollTable
+
