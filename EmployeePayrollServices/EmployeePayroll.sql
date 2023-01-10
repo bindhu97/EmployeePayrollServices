2c57075edc10f660a442da6ddcd64f35d226c8c1
bindhu97 <binduvishwakarma97@gmail.com>
2023-01-10 23:55:53 +0530

    [Bindhu]Add:Ability to create Employee Data

--- EmployeePayrollServices/EmployeePayroll.sql
+++ EmployeePayrollServices/EmployeePayroll.sql
@@ -3,3 +3,6 @@ Create Database EmployeePayroll;
 Create Table EmployeePayrollTable(ID bigint Identity(1,1)Primary Key, Name varchar(100), Salary bigint, Start date)
 
 select * from EmployeePayrollTable
+
+Insert into EmployeePayrollTable values ('Alex', 25000, '2020-02-05'), ('Sunil', 30000, '2018-01-01'), ('Meena', 35000, '2019-05-05'), ('Asha', 40000, '2018-08-04');
+
